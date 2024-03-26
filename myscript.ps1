3c2e7cf208b9b27562f48d3663d9a6cc3b721728
sinh.van <sinh.van@somwhere.com>
2024-03-26 20:11:11 +1100

    script

--- myscript.ps1
+++ myscript.ps1
@@ -1,5 +1,5 @@
 # Get an access token for managed identities for Azure resources
-$response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Headers @{Metadata="true"}
+$response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Headers @{Metadata="true"} -UseBasicParsing
 $content = $response.Content | ConvertFrom-Json
 $access_token = $content.access_token
 echo "The managed identities for Azure resources access token is $access_token"
